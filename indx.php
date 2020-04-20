ba4b865c1433be611c77fe186e6b5a6c625b169a
shawranjit <63969929+shawranjit@users.noreply.github.com>
2020-04-20 08:35:19 +0530

    Add files via upload

---
+++ indx.php
@@ -0,0 +1,23 @@
+<html>
+<title>BIO DATA</title>
+<body bgcolor="pink">
+<center><h1><U> BIO DATA</U></h1></center>
+<!--https://www.toolfk.com/tool-convert-php-->
+<!--./ngrok http 8080-->
+<!--https://accounts.google.com/SignUpMobile-->
+
+<!--php -S localhost:8080 -t /sdcard/www/public-->
+<center><img src="https://unlimitedet.000webhostapp.com/aman.jpg" height="200" width="200"</center>
+<center><p><font color='indigo'>Name:- Aadarsh Shaw</p></center>
+<center><p><font color='red'>Father's Name:- Ranjit Shaw</p></center>
+<center><p> Address:- sthirpara mathpara,near GEL CHURCH</p></center>
+<center><p><font color='green'>Pin Code:-743127</p></center>
+<center><p> Country:-India</p></center>
+<center><p> District:-near Sthirpara, Kankinara, Jagatdal, Bhatpara, West Bengal</p></center>
+<center><p><font color='green'>Email Id:-shawaadarsh5432@gmail.com</p></center>
+<center><p><font color='red'>City:-Kolkata</p></center>
+<center><p><font color='dark crimson'>School:-Jagatdal Chasmah-I- Rahmat High School</p></center>
+<center><p><font color='dark green'>Hobbies:-gaming</p></center>
+<center><h1><U>Objective:-Do not believe in religion and caste.  I know that humanity is the biggest religion.</U></h1></center>
+</body>
+</html>
